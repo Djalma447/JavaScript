60d0f4c63b23135fdda18d644e99dabd8a9c6a7a
Djalma <57427879+Djalma447@users.noreply.github.com>
2024-07-20 23:55:59 -0300

    JavaScript - aula 13 repetições pt.1

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do{
+console.log(`Passo ${c}`)
+    c++
+}while (c <= 100)
+
+
+/*
+var c = 1
+do{
+console.log(`Passo ${c}`)
+    c++
+}while (c <= 100)
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
